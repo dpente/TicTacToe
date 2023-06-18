1c7162e065efc4f50760b4b8c85d1f30e5c16a83
dpente <darrylpentecost@gmail.com>
2023-06-18 22:59:06 +1200

    build: draw condition

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,12 +9,14 @@ public class GameController : MonoBehaviour
     public GameObject gameOverPanel;
     public Text gameOverText;
     private string playerSide;
+    private int moveCount;
 
     private void Awake()
     {
         gameOverPanel.SetActive(false);
         SetGameControllerRefOnButtons();
         playerSide = "X";
+        moveCount = 0;
     }
 
     private void SetGameControllerRefOnButtons()
@@ -32,6 +34,8 @@ public class GameController : MonoBehaviour
 
     public void EndTurn()
     {
+        moveCount++;
+
         if((buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) ||
            (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide) ||
            (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide) ||
@@ -44,6 +48,10 @@ public class GameController : MonoBehaviour
             gameOver();
         }
 
+        if(moveCount >= 9)
+        {
+            setGameOverText("Draw!");
+        }
         changeSides();
     }
 
@@ -54,11 +62,18 @@ public class GameController : MonoBehaviour
 
     private void gameOver()
     {
+        
         for(int i =0; i < buttonList.Length; i++)
         {
             buttonList[i].GetComponentInParent<Button>().interactable = false;
         }
+        
+        setGameOverText(playerSide + " Wins!");
+    }
+
+    private void setGameOverText(string condition)
+    {
         gameOverPanel.SetActive(true);
-        gameOverText.text = playerSide + " Wins!";
+        gameOverText.text = condition;
     }
 }
